04b709e85493763cfdc1158c4992925024227fb9
Halyna <galacuvaeva4@gmail.com>
2026-03-08 20:00:08 +0200

    add loading

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,17 +2,43 @@ import Section from "../Section/Section";
 import Container from "../Container/Container";
 import Form from "../Form/Form";
 import { getPhotos } from "../../services/photos";
-
+import { useState } from "react";
+import type { Photo } from "../../types/photo";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function App() {
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
   const searchHandler = async (search: string) => {
-    const photos = await getPhotos(search);
-    console.log(photos);
+    try {
+      setIsLoading(true);
+      setIsError(false);
+      setPhotos([]);
+      const fetchedPhotos = await getPhotos(search);
+      if (fetchedPhotos.length === 0) {
+        toast.error('За вашим запитом немає даних');
+        return;
+      }
+      setPhotos(fetchedPhotos);
+    }
+    catch {
+      setIsError(true);
+      toast.error('Щось пішло не так');
+    }
+    finally {
+      setIsLoading(false);
+    }
   }
   return (
     <>
+      
       <Section>
-        <Container><Form onSubmit = {searchHandler} /></Container>
+        <Container>
+          <Form onSubmit={searchHandler} />
+          <Toaster />
+        </Container>
    
       </Section>
     </>
